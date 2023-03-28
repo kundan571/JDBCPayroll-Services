3d03eca3541a947fdbdb2a3e5207c2c00ec2fece
kundan kumar <kundanchauhan1997@gmail.com>
2023-03-29 00:59:41 +0530

    Added: Ability to update basic pay salary of employee

--- src/main/java/com/jdbcemploye_payroll/EmployeePayrollDBConnection.java
+++ src/main/java/com/jdbcemploye_payroll/EmployeePayrollDBConnection.java
@@ -4,7 +4,7 @@ import java.sql.*;
 
 public class EmployeePayrollDBConnection {
 
-    Connection con;
+    static Connection con;
     PreparedStatement ps;
     ResultSet rs;
 
@@ -22,18 +22,27 @@ public class EmployeePayrollDBConnection {
         Statement statement = con.createStatement();
         String query = "Select * from Employee_Payroll";
         rs = statement.executeQuery(query);
-        while (rs.next()){
+        while (rs.next()) {
             System.out.println(rs.getInt("empid") + " " + rs.getString("name") + " "
-              + rs.getString("gender") + " " + rs.getInt("salary") + " " + rs.getDate("start_date") +
-                    " " + rs.getString("Address"));
+                    + rs.getString("gender") + " " + rs.getInt("salary") + " " + rs.getDate("start_date") +
+                    " " + rs.getString("Address") + " " + rs.getString("basic_pay"));
         }
-        con.close();
-        System.out.println("Connection closed:");
     }
+
+    void update() throws SQLException {
+        Statement statement = con.createStatement();
+        String query = "update Employee_payroll set basic_pay = '3000000' Where name = 'satya'";
+        int rowsAffected = statement.executeUpdate(query);
+        System.out.println("Rows Affected: " + rowsAffected);
+    }
+
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
         System.out.println("Welcome to Employee Payroll DataBase:");
         EmployeePayrollDBConnection empDBConnect = new EmployeePayrollDBConnection();
         empDBConnect.databaseConnect();
+        empDBConnect.update();
         empDBConnect.display();
+        con.close();
+        System.out.println("Connection closed:");
     }
 }
