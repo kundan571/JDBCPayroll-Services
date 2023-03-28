0a9ed1883c1edf30a878a8f9020373dfba6a015a
kundan kumar <kundanchauhan1997@gmail.com>
2023-03-28 23:02:57 +0530

    Added: Connection established with the database

--- src/main/java/com/jdbcemploye_payroll/EmployeePayrollDBConnection.java
+++ src/main/java/com/jdbcemploye_payroll/EmployeePayrollDBConnection.java
@@ -1,7 +1,27 @@
 package com.jdbcemploye_payroll;
 
+import java.sql.*;
+
 public class EmployeePayrollDBConnection {
-    public static void main(String[] args) {
+
+    Connection con;
+    PreparedStatement ps;
+    ResultSet rs;
+
+    void databaseConnect() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        String url = "jdbc:mysql://localhost:3306/payroll_service";
+        String username = "root";
+        String password = "Password@121";
+        System.out.println("Connecting database...");
+        con = DriverManager.getConnection(url, username, password);
+        System.out.println("Connection Successful:");
+    }
+
+
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
         System.out.println("Welcome to Employee Payroll DataBase:");
+        EmployeePayrollDBConnection empDBConnect = new EmployeePayrollDBConnection();
+        empDBConnect.databaseConnect();
     }
 }
